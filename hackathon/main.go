0f705e2ee9d4f725c500992acb1f938eede1c3a0
Yuki-Ikami <ikami-yuki544@g.ecc.u-tokyo.ac.jp>
2023-06-08 21:33:29 +0900

    add allfiles

--- hackathon/main.go
+++ hackathon/main.go
@@ -64,12 +64,13 @@ type Edit struct {
 var db *sql.DB
 
 func init() {
-	mysqlUser := "test_user"
-	mysqlUserPwd := "password"
-	mysqlDatabase := "test_database"
+	mysqlUser := os.Getenv("MYSQL_USER")
+    mysqlPwd := os.Getenv("MYSQL_PWD")
+    mysqlHost := os.Getenv("MYSQL_HOST")
+    mysqlDatabase := os.Getenv("MYSQL_DATABASE")
 
-	// ①-2
-	_db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@(localhost:3306)/%s", mysqlUser, mysqlUserPwd, mysqlDatabase))
+    connStr := fmt.Sprintf("%s:%s@%s/%s", mysqlUser, mysqlPwd, mysqlHost, mysqlDatabase)
+    db, err := sql.Open("mysql", connStr)
 	if err != nil {
 		log.Fatalf("fail: sql.Open, %v\n", err)
 	}
